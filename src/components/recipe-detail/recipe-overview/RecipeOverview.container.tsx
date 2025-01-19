3694d77522cfc8a87daac78dc56c19e52ef1aa64
Genichi Ishii <gishii507@gmail.com>
2025-01-19 12:05:26 +0900

    fix: data fetch

--- src/components/recipe-detail/recipe-overview/RecipeOverview.container.tsx
+++ src/components/recipe-detail/recipe-overview/RecipeOverview.container.tsx
@@ -9,23 +9,25 @@ type RecipeOverviewContainerProps = {
 export const RecipeOverviewContainer = async ({
   recipeId,
 }: RecipeOverviewContainerProps) => {
-  const res = await getRecipeById(recipeId);
-  const { data } = await HasuraGetRecipeById({
-    recipeId,
-  });
+  const [microCmsResponse, hasuraResponse] = await Promise.all([
+    getRecipeById(recipeId),
+    HasuraGetRecipeById({ recipeId }),
+  ]);
 
   return (
     <RecipeOverview
       image={{
-        src: res.image.url,
-        alt: res.name,
+        src: microCmsResponse.image.url,
+        alt: microCmsResponse.name,
       }}
       recipeMetaInfo={{
-        recipeName: res.name,
-        deliciousCount: data.nozomis_recipes_schema_recipes[0].delicious_count,
-        totalView: data.nozomis_recipes_schema_recipes[0].total_view,
-        recipeDescription: res.description,
-        recipeTags: res.tags.map((tag) => {
+        recipeName: microCmsResponse.name,
+        deliciousCount:
+          hasuraResponse.data.nozomis_recipes_schema_recipes[0].delicious_count,
+        totalView:
+          hasuraResponse.data.nozomis_recipes_schema_recipes[0].total_view,
+        recipeDescription: microCmsResponse.description,
+        recipeTags: microCmsResponse.tags.map((tag) => {
           return {
             id: tag.id,
             name: tag.name,
