30065218005ee621196d8fa861dd1d662697edca
Genichi Ishii <gishii507@gmail.com>
2025-03-28 00:18:51 +0900

    style: Adjust layout of Home component for improved spacing
    - Add top margin to the main container in the Home component for better visual separation
    - Enhance overall layout consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
   return (
     <>
       {/* <Header /> */}
-      <div className='flex flex-col items-center'>
+      <div className='flex flex-col items-center mt-8'>
         <HeroCarousel />
       </div>
 
